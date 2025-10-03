575a81151c89a400ad4ca2fcab63cda3c0ea9e72
vssj-codes <vssjcodes@gmail.com>
2025-10-03 18:02:59 +0530

    ch04: change password

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,6 +9,16 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    path(
+        'password-change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),  name='password_change_done'
+    ),
+
     path('', views.dashboard, name='dashboard'),
 
 ]
