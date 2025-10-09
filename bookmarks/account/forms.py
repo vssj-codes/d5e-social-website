0106668ed627484aaa1ae829d1aaf26e952e7153
vssj-codes <vssjcodes@gmail.com>
2025-10-09 08:44:43 +0530

    ch05: custom auth backend

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -10,33 +10,40 @@ class LoginForm(forms.Form):
 
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput
-    )
-    password2 = forms.CharField(
-        label='Repeat password',
-        widget=forms.PasswordInput
-    )
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput)
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'first_name', 'email']
+        fields = ["username", "first_name", "email"]
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
+        if cd["password"] != cd["password2"]:
             raise forms.ValidationError("Passwords don't match.")
-        return cd['password2']
+        return cd["password2"]
+
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        if get_user_model().objects.filter(email=data).exists():
+            raise forms.ValidationError("Email already in use.")
+        return data
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
-        fields = ['first_name', 'last_name', 'email']
+        fields = ["first_name", "last_name", "email"]
+
+    def clean_email(self):
+        data = self.cleaned_data["email"]
+        qs = get_user_model().objects.exclude(id=self.instance.id).filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError("Email already in use.")
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['date_of_birth', 'photo']
+        fields = ["date_of_birth", "photo"]
