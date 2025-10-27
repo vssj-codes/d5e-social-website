3c869d81f4fcf868b76bae7baa1b1a60df2f7e75
vssj-codes <vssjcodes@gmail.com>
2025-10-27 12:41:02 +0530

    ch07: building a follow system

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -15,3 +16,39 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'Profile of {self.user.username}'
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_from_set',
+        on_delete=models.CASCADE,
+    )
+    user_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='rel_to_set',
+        on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+        ordering = ['-created']
+
+    def __str__(self):
+        return f'{self.user_from} follows {self.user_to}'
+
+
+# Add following field to User dynamically
+user_model = get_user_model()
+user_model.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contact,
+        related_name='followers',
+        symmetrical=False,
+    ),
+)
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_POST
+from django.shortcuts import get_object_or_404, render
 from django.contrib import messages
+
+User = get_user_model() 
 
 from .forms import (
     LoginForm,
@@ -10,7 +13,7 @@ from .forms import (
     UserEditForm,
     UserRegistrationForm,
 )
-from .models import Profile
+from .models import Contact,Profile
 
 
 def user_login(request):
@@ -86,3 +89,45 @@ def edit(request):
         "account/edit.html",
         {"user_form": user_form, "profile_form": profile_form},
     )
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request,
+        'account/user/list.html',
+        {'section': 'people', 'users': users},
+    )
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request,
+        'account/user/detail.html',
+        {'section': 'people', 'user': user},
+    )
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    user_to=user
+                ).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
